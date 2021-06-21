d85c34fb77a59a1363550643928ce83c626dd0af
ovenbakedgoods <ovenbakedgoods88@gmail.com>
2021-06-20 21:56:42 -0500

    Updating last changes

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,11 +5,17 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  console.log(props)
+  console.log(props.posts[0].username)
   return (
+    
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {    
+        props.posts.map((post, i) => (
+          <Post key={"Posts-Map-" + i} post={post} likePost={props.likePost} />
+        ))}
     </div>
   );
 };
